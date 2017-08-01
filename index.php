bace2d8fc9745c174169cfb34620018c383a5ace
susan8899 <su.susan89@gmail.com>
2017-08-01 12:36:52 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<head>
+	<title>Hangman</title>
+	 
+	 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <link href="test.css" rel="stylesheet" type="text/css" />
+</head>
+<body id='content1'>
+	 
+	
+        
+ <div id = 'nav'>
+   <ul>
+     <li><a href="test.php">Home</a></li>
+     <li><a href="http://www.wikihow.com/Play-Hangman">About</a></li>
+  
+      
+   </ul>
+</div><br><br><br>
+<div id = 'head' ><h1>You Want To Play Hangman</h1></div><br>
+<div id ='p1'>Please choose your category to start play</div>
+
+       <div id='try'>  Category: <select name="category" onchange="location= this.value;">
+    <option value="test.php">Home</option>      
+  <option value="state.php">US State</option>
+  <option value="country.php">Country Name</option>
+   <option value="geo.php">Geography</option>
+    <option value="color.php">Colors</option>
+     <option value="csc.php">Computer Science</option>
+
+</select></div> <br><br><br>
+<div id='p1'>Here is our most popular list</div>
+
+<div id='l1'>
+<a href="state.php">List of U.S states</a>
+</div>
+<div id='l2'>
+<a href="country.php">List of Country</a>
+</div>
+<div id='l3'>
+<a href="csc.php">Computer Science</a>
+</div>
+
+
+</body>
+</html>
+
---
+++ state.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<head>
+	<title>Hangman</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link href="test.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body id ='content2'>
+
+ <a href= "test.php">Home</a><br><br>
+
+<?php
+echo "You have chosen US State";
+$Group = "US State";
+//ALABAMA n=0
+//ARIZONA n=1
+//FLORIDA n=2
+//GEORGIA  n=3
+//LOWA    n=4
+//IDAHO   n=5
+//KENTUCKY n=6
+//IDIANA    n=7
+//MARYLAND   n=8
+//KANSAS     n=9
+//ILLINOIS   n=10
+//MAINE      n=11
+//MINNESOTA  n=12
+//NEVADA      n=13
+//MONATA      n=14
+//NEW HAMPSHIRE   n=15
+//NEW MAXICO      n=16
+//PENNSYLVANIA     n=17
+//UTAH             n=18         
+//WYOMING          n=19
+//WISCONSIN        n=20
+//RHODE ISLAND     n=21
+//VERMONT          n=22
+//OREGON            n=23
+$item = "ALABAMA
+ARIZONA
+FLORIDA
+GEORGIA
+IOWA
+IDAHO
+KENTUCKY
+IDIANA
+MARYLAND
+KANSAS
+ILLINOIS
+MAINE
+MINNESOTA
+NEVADA
+MONATA
+NEW HAMPSHIRE
+NEW MAXICO
+PENNSYLVANIA
+UTAH
+WYOMING
+WISCONSIN
+RHODE ISLAND
+VERMONT
+OREGON";
+
+$special_letters = " -.,;!?%&0123456789";
+
+$char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// len_char is equal to the length of $char
+
+// isset() checks whether a variable is set or not
+//TRUE if variable exists and has value not equal to NULL
+
+// $_GET gets variable n
+$len_char = strlen($char);
+
+if(isset($_GET["num"])) $num=$_GET["num"]; // if the chosen word exists then num gets assigned that chosen word
+
+if(isset($_GET["characters"])) $characters=$_GET["characters"];
+// if the letters chosen exist then $characters get assigned that letter
+
+if(!isset($characters)) $characters=""; //if $characters does not exists then $character gets assigned an empty string
+
+
+#The filename of the currently executing script, relative to the document root. For instance, $_SERVER['PHP_SELF'] in a script at the address 
+
+if(isset($PHP_SELF)) $file=$PHP_SELF;
+else $file=$_SERVER["PHP_SELF"];
+
+$hlinks="";
+
+
+$count=6;	 // maximum number of wrong			
+
+$item = strtoupper($item);  //strtoupper converts all characters to uppercase
+$s_words = explode("\n",$item); //The explode() function is used to split a   string. Sets the boundary string within the input string
+
+
+
+$all_letters=$characters.$special_letters;
+$total_wrong = 0;
+
+echo "<p><b>Hangman Game</b> &nbsp;  &nbsp; &nbsp; &nbsp; <b>Subject:  <b>$Group</b><br>\n";
+
+if (!isset($num)) { ///if num does not exist, then n will be assigned to a random word from the list
+
+  $num = rand(1,count($s_words)) - 1; //// between 1 and number of words available, -1
+}
+$word_line=""; //removes characters/whitespace from both sides of the random word
+
+$c_word = trim($s_words[$num]);
+$finish = 1;         // finish is set to 1/true
+
+//Sets up the black spaces for the random word 
+for ($a=0; $a < strlen($c_word); $a++)
+{
+  if (strstr($all_letters, $c_word[$a])) 
+    //strstr() function searches for the first occurrence of a string inside another string.
+  {
+    if ($c_word[$a]==" ") $word_line.="&nbsp; ";  // if a position in word is in $all_letters(all_letters are the letters not in $char) 
+    else $word_line.=$c_word[$a];  //  $word_line gets the ath letter in the word ex. ! - is printed instead of __
+  } 
+  else { 
+    $word_line.="_<font size=1>&nbsp;</font>"; //.=,Appends $txt2 to $txt1
+    //if a positions is in $char, a __ is created
+    $finish = 0;     // finish is set to false, game begins
+      
+  } 
+
+}
+
+if (!$finish)
+{
+
+  for ($c=0; $c<$len_char; $c++)  // if alphabet letter you picked is in the list of letters 
+  {
+    if (strstr($characters, $char[$c]))  // then the char letter is searched in the random word 
+    {
+      if (strstr($s_words[$num], $char[$c])) //if it exists the link will change to bold 
+      {
+        $hlinks .= "\n<b>$char[$c]</b> "; //if char is not in the random word then the link will change to red and $total_wrong will be inc
+      }
+      else { 
+        $hlinks .= "\n<font color=\"red\">$char[$c] </font>"; //if char letter you picked is not in the list of letters then
+        //url is changed to include that alpha
+         $total_wrong++; 
+      }
+    
+    }
+    else
+    {
+     $hlinks .= "\n<A HREF=\"$file?characters=$char[$c]$characters&num=$num\">$char[$c]</A> "; 
+   }
+  
+  }
+  // if you got one wrong then img displays 
+  $numwrong=$total_wrong; 
+
+   
+  if ($numwrong>6) $numwrong=6;
+  echo "\n<p><br>\n<IMG SRC=\"Hangman-$numwrong.png\" ALIGN=\"MIDDLE\" BORDER=0 WIDTH=150 HEIGHT=150 >\n";
+
+//if you use up all your 6 guesses then this displays
+  if ($total_wrong >= $count)
+  {
+    $num++;  //next random word gets inc
+    if ($num>(count($s_words)-1)) $num=0;
+
+    echo "<br><br><h1><font size=5>\n$word_line</font></h1>\n";
+    echo "<p><br><font color=\"red\"><big>SORRY, YOU ARE HANGED!!!</big></font><br><br>";
+    echo "\n<p><br>\n<IMG SRC=\"loser.jpeg\" ALIGN=\"MIDDLE\" BORDER=0 WIDTH=300 HEIGHT=300 >\n";
+
+    if (strstr($c_word, " ")) $term="words"; else $term="word";
+    echo "The $term was \"<B>$c_word</B>\"<BR><BR>\n";
+
+    echo "<A HREF=$file?num=$num>Play again.</A>\n\n"; //links Play again link to itsef
+  }
+  // if you havent used up your 6 guesses, it updates  " number of wrong guesses left:"
+  else
+  {
+    echo " &nbsp; You Have <b>" .($count-$total_wrong). " Guesses Left "."</b><br>\n";
+    echo "<h1><font size=5>\n$word_line</font></h1>\n";
+    echo "<p><br>Please choose a letter:<br><br>\n";
+    echo "$hlinks\n";
+  }
+}
+else  //you got the word right
+{
+  $num++;	# get next word
+  if ($num>(count($s_words)-1)) $num=0;
+  echo "\n<p><br>\n<IMG SRC=\"great_job.gif\" ALIGN=\"MIDDLE\" BORDER=0 WIDTH=500 HEIGHT=500 >\n";
+  echo "<br><br><h1><font size=5>\n$word_line</font></h1>\n";
+  echo "<p><br><br><b>Congratulations!!! &nbsp;YOU WON!!!</b><br><br><br>\n";
+  echo "<A HREF=$file?num=$num>Play again</A>\n\n";
+}
+?>
+</body>
+</html>
